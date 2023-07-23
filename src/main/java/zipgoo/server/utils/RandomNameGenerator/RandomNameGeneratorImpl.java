b7ec3517e4af0090ac5ce0e0dadf874c5b69f969
JunHyeon <dlwnsgus07@naver.com>
2023-07-23 23:50:42 +0900

    fix : null이 추출되던 버그 수정

--- src/main/java/zipgoo/server/utils/RandomNameGenerator/RandomNameGeneratorImpl.java
+++ src/main/java/zipgoo/server/utils/RandomNameGenerator/RandomNameGeneratorImpl.java
@@ -1,33 +1,46 @@
 package zipgoo.server.utils.RandomNameGenerator;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import zipgoo.server.domain.User;
+import zipgoo.server.repository.UserRepository;
+
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.Random;
 
 @Slf4j
+@Service
 public class RandomNameGeneratorImpl implements RandomNameGeneratorUtil {
+    private final UserRepository userRepository;
     public static final String ANIMAL_FILE_URL = "/Users/dlwnsgus07/Desktop/Project/server/src/main/java/zipgoo/server/utils/RandomNameGenerator/animalName.json";
     public static final String ADJECTIVE_FILE_URL = "/Users/dlwnsgus07/Desktop/Project/server/src/main/java/zipgoo/server/utils/RandomNameGenerator/adjectiveName.json";
     static Integer MAX_ANIMAL_COUNT = 41495;
     static Integer MAX_ADJECTIVE_COUNT = 43;
-    JSONObject adjectiveObj;
-    JSONObject animalObj;
-    JSONParser parser = new JSONParser();
+    private JSONObject adjectiveObj;
+    private JSONObject animalObj;
 
-    public RandomNameGeneratorImpl() {
+    @Autowired
+    public RandomNameGeneratorImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
         try {
             FileReader animalReader = new FileReader(ANIMAL_FILE_URL);
             FileReader reader = new FileReader(ADJECTIVE_FILE_URL);
+            JSONParser parser = new JSONParser();
             animalObj = (JSONObject) parser.parse(animalReader);
             adjectiveObj = (JSONObject) parser.parse(reader);
         } catch (IOException | ParseException e) {
             log.error(e.getMessage());
         }
     }
+
     /**
      * 형용사를 바탕으로 랜덤으로 가져옴
      * @return 형용사
@@ -57,6 +70,16 @@ public class RandomNameGeneratorImpl implements RandomNameGeneratorUtil {
      */
     @Override
     public String getFullName() {
+        String name = getName();
+        Optional<User> user = userRepository.findByNickname(name);
+        while (user.isPresent()){
+            name = getName();
+            user = userRepository.findByNickname(name);
+        }
+        return name;
+    }
+
+    private String getName() {
         return getFirstName() + " " + getSecondName();
     }
 }
