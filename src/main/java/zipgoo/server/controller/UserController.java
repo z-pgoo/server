fcf09b34bbcf9fe8d1f79801d77d76a0b070b2e9
JunHyeon <dlwnsgus07@naver.com>
2023-07-23 23:50:53 +0900

    feat : 컨트롤러 등록

--- src/main/java/zipgoo/server/controller/UserController.java
+++ src/main/java/zipgoo/server/controller/UserController.java
@@ -10,6 +10,8 @@ import zipgoo.server.dto.UserSignUpDto;
 import zipgoo.server.jwt.JwtService;
 import zipgoo.server.repository.UserRepository;
 import zipgoo.server.service.UserService;
+import zipgoo.server.utils.RandomNameGenerator.RandomNameGeneratorImpl;
+import zipgoo.server.utils.RandomNameGenerator.RandomNameGeneratorUtil;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,6 +20,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final RandomNameGeneratorUtil randomNameGenerator;
 
     @PostMapping("/sign-up")
     public ResponseEntity<Map<String, Object>> signUp(@RequestBody UserSignUpDto userSignUpDto) throws Exception{
@@ -28,7 +31,13 @@ public class UserController {
     public ResponseEntity<Map<String, Object>> login(@RequestBody LoginDto loginDto) throws Exception{
         return userService.login(loginDto);
     }
-
-
+    @PostMapping("/random-name")
+    public String randName(){
+        return randomNameGenerator.getFullName();
+    }
+    @GetMapping("/jwt-test")
+    public String jwtTest(){
+        return "jwtTest 요청 성공";
+    }
 
 }
