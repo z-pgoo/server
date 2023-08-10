2a5d6b853e051d57669f92a347c4a783eb34bf69
JunHyeon <dlwnsgus07@naver.com>
2023-08-10 09:04:09 +0900

    fix : entity

--- src/main/java/zipgoo/server/domain/Post.java
+++ src/main/java/zipgoo/server/domain/Post.java
@@ -4,15 +4,18 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Board {
+public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;     // 게시물 아이디
@@ -21,20 +24,31 @@ public class Board {
     private String content;    // 내용
     @OneToOne
     private User user;// 작성자
-    private int hits;    // 조회수
+    private int saves;    // 조회수
     private int recommend; // 추천 수
 
     @Embedded
     private Address address;
+
+    @CreatedDate
     private final LocalDateTime createDate = LocalDateTime.now(); // 생설일
+    @LastModifiedDate
     private LocalDateTime modifiedDate; // 수정일
+    @OneToMany(mappedBy = "posts", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    @OrderBy("id asc")
+    private List<Comment> commentList;
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
     @Builder
-    public Board(String title, String content, User user, int hits, int recommend, Address address){
+    public Post(String title, String content, User user, Address address){
         this.title = title;
         this.content = content;
         this.user = user;
-        this.hits = hits;
-        this.recommend = recommend;
+        this.saves = 0;
+        this.recommend = 0;
         this.address = address;
     }
 }
--- src/main/java/zipgoo/server/repository/BoardRepository.java
+++ src/main/java/zipgoo/server/repository/BoardRepository.java
@@ -1,16 +1,15 @@
 package zipgoo.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import zipgoo.server.domain.Board;
+import zipgoo.server.domain.Post;
 import zipgoo.server.domain.Category;
-import zipgoo.server.domain.User;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
-    Optional<List<Board>> findBoardsByTitle(String title);
-    Optional<List<Board>> findBoardsByUser_Nickname(String nickName);
-    Optional<List<Board>> findBoardsByAddress_City(String city);
-    Optional<List<Board>> findBoardsByCategory(Category category);
+public interface BoardRepository extends JpaRepository<Post, Long> {
+    Optional<List<Post>> findBoardsByTitle(String title);
+    Optional<List<Post>> findBoardsByUser_Nickname(String nickName);
+    Optional<List<Post>> findBoardsByAddress_City(String city);
+    Optional<List<Post>> findBoardsByCategory(Category category);
 }
